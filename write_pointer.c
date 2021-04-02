65c785483e8356eca7259e22cfe3b092d7385aa2
김동휘 <v2008v@daum.net>
2021-04-02 15:32:49 +0900

    end of pointer

--- write_pointer.c
+++ write_pointer.c
@@ -6,7 +6,7 @@
 /*   By: donghwik <donghwik@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 17:30:37 by donghwik          #+#    #+#             */
-/*   Updated: 2021/04/02 15:28:16 by donghwik         ###   ########.fr       */
+/*   Updated: 2021/04/02 15:32:36 by donghwik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int     print_pointer(long long n, t_info info, int radix, int *result)
 {
     int         len;
 
-    len = ft_numlen(n, radix);
+    len = ft_numlen(n, radix) + 2;
 	if (info.flag == 0)
 	{
 		print_leftize_pointer(n, info, radix, result);
@@ -32,6 +32,8 @@ int     print_pointer(long long n, t_info info, int radix, int *result)
 	// 	wc1('0', result);
 	// 	len++;
 	// }
+	write(1, &"0x", 2);
+	(*result) += 2;
 	write_positive_integer_lower(n, radix, result);
     return (1);
 }
@@ -40,12 +42,14 @@ int		print_leftize_pointer(long long n, t_info info, int radix, int *result)
 {
 	int         len;
 
-    len = ft_numlen(n, radix);
+    len = ft_numlen(n, radix) + 2;
 	// while (9 - len > 0)
 	// {
 	// 	wc1('0', result);
 	// 	len++;
 	// }
+	write(1, &"0x", 2);
+	(*result) += 2;
 	write_positive_integer_lower(n, radix, result);
 	while (info.width - len > 0)
 	{
