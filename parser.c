835797021736c8b25d569a2c32f84864b9012c77
김동휘 <v2008v@daum.net>
2021-04-02 14:32:07 +0900

    hello world

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: donghwik <donghwik@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 17:30:26 by donghwik          #+#    #+#             */
-/*   Updated: 2021/04/02 14:30:39 by donghwik         ###   ########.fr       */
+/*   Updated: 2021/04/02 14:31:57 by donghwik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,7 +87,8 @@ int     preci_proc(const char **format, va_list *ap, t_info *temp)
             if (ret < 0)
             {
                 temp->print_zero = 1;
-                return (0);
+                // chagne point
+                return (ret);
             }
             if (ret == 0)
                 temp->print_zero = 0;
