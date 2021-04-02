73cd9312d175ea4565b97329efd27b0d52bbd651
김동휘 <v2008v@daum.net>
2021-04-02 10:44:52 +0900

    blank problem solved

--- write_num.c
+++ write_num.c
@@ -6,7 +6,7 @@
 /*   By: donghwik <donghwik@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 17:30:30 by donghwik          #+#    #+#             */
-/*   Updated: 2021/04/02 10:40:37 by donghwik         ###   ########.fr       */
+/*   Updated: 2021/04/02 10:44:28 by donghwik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,9 +58,9 @@ int     print_integer(long long n, t_info info, int radix, int *result)
         info.precision--;
         wc1('0', result);
     }
-    if (n == 0 && info.print_zero == 0)
-        wc1(' ', result);
-    else
+    if (!(n == 0 && info.print_zero == 0))
+        //wc1(' ', result);
+    //else
         write_positive_integer(n, radix, result);
     return (1);
 }
