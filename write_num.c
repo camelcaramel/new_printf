254a5abafc221dd95102a623e515be4e6ab5667a
김동휘 <v2008v@daum.net>
2021-04-02 12:07:04 +0900

    finally

--- write_num.c
+++ write_num.c
@@ -6,7 +6,7 @@
 /*   By: donghwik <donghwik@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 17:30:30 by donghwik          #+#    #+#             */
-/*   Updated: 2021/04/02 12:04:29 by donghwik         ###   ########.fr       */
+/*   Updated: 2021/04/02 12:06:45 by donghwik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ int     print_integer(long long n, t_info info, int radix, int *result)
 		return (print_leftize_integer(n, info, radix, result));
     if (n < 0)
         info.width--;
-    if (n < 0 && (n *= -1) > 0 && info.flag == 2)
+    if (n < 0 && info.flag == 2)
         wc1('-', result);
     while (info.width - info.precision > 0 && info.width - len > 0)
     {
