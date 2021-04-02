97d8634f6970efaff4196d7b16aef41a002a9002
김동휘 <v2008v@daum.net>
2021-04-02 15:27:01 +0900

    hello world

--- write_hex.c
+++ write_hex.c
@@ -1,34 +1,5 @@
 #include "ft_printf.h"
 
-// int     print_integer_hex(long long n, t_info info, int radix, int *result)
-// {
-//     int         	len;
-// 	unsigned int	number;
-
-// 	number = (unsigned int)n;
-// 	len = ft_numlen(number, radix);
-// 	if (info.flag == 0)
-// 		return (print_leftize_integer(number, info, radix, result));
-//     while (info.width - info.precision > 0 && info.width - len > 0)
-//     {
-//         if (info.flag == 2)
-// 			wc1(' ', result);
-// 		else	
-// 			wc1(' ', result);
-//         info.width--;
-//     }
-//     while (info.precision - len > 0)
-//     {
-//         info.precision--;
-//         wc1(' ', result);
-//     }
-// 	if (radix == 15)
-// 		write_positive_integer_lower(number, radix + 1, result);
-// 	else
-//     	write_positive_integer(number, radix, result);
-//     return (1);
-// }
-
 int     print_integer_hex(long long n, t_info info, int radix, int *result)
 {
     int         len;
--- write_pointer.c
+++ write_pointer.c
@@ -6,7 +6,7 @@
 /*   By: donghwik <donghwik@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 17:30:37 by donghwik          #+#    #+#             */
-/*   Updated: 2021/03/29 17:33:36 by donghwik         ###   ########.fr       */
+/*   Updated: 2021/04/02 15:26:22 by donghwik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@ int     print_pointer(long long n, t_info info, int radix, int *result)
 		wc1(' ', result);
         info.width--;
     }
-	while (9 - len > 0)
-	{
-		wc1('0', result);
-		len++;
-	}
+	// while (9 - len > 0)
+	// {
+	// 	wc1('0', result);
+	// 	len++;
+	// }
 	write_positive_integer_lower(n, radix, result);
     return (1);
 }
@@ -41,13 +41,13 @@ int		print_leftize_pointer(long long n, t_info info, int radix, int *result)
 	int         len;
 
     len = ft_numlen(n, radix);
-	while (9 - len > 0)
-	{
-		wc1('0', result);
-		len++;
-	}
+	// while (9 - len > 0)
+	// {
+	// 	wc1('0', result);
+	// 	len++;
+	// }
 	write_positive_integer_lower(n, radix, result);
-	while (info.width - 11 > 0)
+	while (info.width - len > 0)
 	{
 		wc1(' ', result);
 		info.width--;
