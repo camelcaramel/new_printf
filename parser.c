5142d501d35a3df5b34fa694b509364451f197c0
김동휘 <v2008v@daum.net>
2021-04-02 12:44:32 +0900

    hello world

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: donghwik <donghwik@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 17:30:26 by donghwik          #+#    #+#             */
-/*   Updated: 2021/04/02 12:40:11 by donghwik         ###   ########.fr       */
+/*   Updated: 2021/04/02 12:44:19 by donghwik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,7 +83,10 @@ int     preci_proc(const char **format, va_list *ap, t_info *temp)
             if (temp->is_va == 1 && temp->flag == 2 && ret > 0)
                 temp->flag = 1;
             if (ret < 0)
+            {
+                temp->print_zero = 1;
                 return (0);
+            }
             if (ret == 0)
             {
                 temp->print_zero = 0;
