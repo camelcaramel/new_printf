2ecb34dd9ddf655c5d12c579e1640a42b2129bd5
김동휘 <v2008v@daum.net>
2021-04-12 20:40:07 +0900

    hello world

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: donghwik <donghwik@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 17:30:55 by donghwik          #+#    #+#             */
-/*   Updated: 2021/04/12 20:33:30 by donghwik         ###   ########.fr       */
+/*   Updated: 2021/04/12 20:39:55 by donghwik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,7 +128,16 @@ int		format_print(const char **format, va_list *ap, int *result)
 	temp.flag = flag_proc(format);
 	temp.width = width_proc(format, ap, &temp);
 	temp.precision = preci_proc(format, ap, &temp);
-	if (temp.flag == 2 && temp.is_dot == 1)
-		temp.flag = 1;
+	// if (temp.flag == 2 && temp.is_dot == 1)
+	// 	temp.flag = 1;
 	return (symbol_switch_number(temp, format, ap, result));
 }
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+// 	ft_printf("ft_printf : |%0*.*i|\n", 4,-4,-12);
+// 	printf("printf : |%0*.*i|\n", 4,-4,-12);
+// 	return 0;
+// }
